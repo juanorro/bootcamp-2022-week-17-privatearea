8202f7f1cdb30288424b0267d68aad6f58f780f1
Flavio Copes <flavio@flaviocopes.com>
2022-07-12 20:01:45 +0200

    Finished project

--- pages/members.js
+++ pages/members.js
@@ -1,8 +1,10 @@
+import { getMarkdown } from 'lib/markdown'
+
 import Head from 'next/head'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
-export default function Home() {
+export default function Members({ markdown }) {
   const router = useRouter()
 
   const { data: session, status } = useSession()
@@ -13,8 +15,12 @@ export default function Home() {
     return null
   }
 
-  if (session) {
-    router.push('/members')
+  if (!session) {
+    router.push('/')
+  }
+
+  if (!session.user.isSubscriber) {
+    router.push('/join')
   }
 
   return (
@@ -28,22 +34,21 @@ export default function Home() {
       <div className='text-center '>
         <h1 className=' mt-20 font-extrabold text-2xl'>Private Area</h1>
 
-        <p className='mt-10'>Join the private area to have access to</p>
-
-        <ol className='mt-10 list-inside list-decimal'>
-          <li>The lyrics book in PDF</li>
-          <li>Exclusive 30% discount on the albums</li>
-          <li>Exclusive access to preorders</li>
-        </ol>
-
-        <p className='mt-10'>Just $5/m</p>
-
-        <div className='mt-10'>
-          <a className='bg-black text-white px-5 py-2' href='/api/auth/signin'>
-            Become a supporter
-          </a>
-        </div>
+        <div
+          className='markdown'
+          dangerouslySetInnerHTML={{ __html: markdown }}
+        />
       </div>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const markdown = await getMarkdown()
+
+  return {
+    props: {
+      markdown,
+    },
+  }
 }
